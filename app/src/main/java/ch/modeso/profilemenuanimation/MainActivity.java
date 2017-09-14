9ac40c6df7e7d53d2c8e81c9e4e66b6e5f32d28f
mahmoudgalal <mahmoudgalal57@yahoo.com>
2017-09-14 11:05:26 +0200

    Some UI Refinements

--- app/src/main/java/ch/modeso/profilemenuanimation/MainActivity.java
+++ app/src/main/java/ch/modeso/profilemenuanimation/MainActivity.java
@@ -3,6 +3,8 @@ package ch.modeso.profilemenuanimation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.List;
 import ch.modeso.mprofileanimatedmenu.widgets.AnimatedMenuItem;
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements ProfileAnimatedMe
 
     private ProfileAnimatedMenu profileAnimatedMenu;
     private TextView selectedOptionText;
+    private ImageView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements ProfileAnimatedMe
         setContentView(R.layout.activity_main);
         profileAnimatedMenu = (ProfileAnimatedMenu) findViewById(R.id.profile_menu);
         selectedOptionText = (TextView) findViewById(R.id.selected_option_item);
+        searchView =  (ImageView) findViewById(R.id.search_view);
         List<AnimatedMenuItem> items  = profileAnimatedMenu.getMenuItems();
 
         for(final AnimatedMenuItem item:items){
@@ -61,9 +65,15 @@ public class MainActivity extends AppCompatActivity implements ProfileAnimatedMe
 
     @Override
     public void onActionEnded(int actionType) {
-        if(actionType== ProfileAnimatedMenu.OnOpenCloseListener.ACTION_TYPE_OPEN)
+        if(actionType== ProfileAnimatedMenu.OnOpenCloseListener.ACTION_TYPE_OPEN) {
+            //Menu opened
             selectedOptionText.setText("Menu opened !");
-        else
+            searchView.setVisibility(View.INVISIBLE);
+        }
+        else {
+            //Menu closed
             selectedOptionText.setText("Menu closed !");
+            searchView.setVisibility(View.VISIBLE);
+        }
     }
 }
